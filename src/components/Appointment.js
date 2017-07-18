5bcd016a09609cc41eead877f85577aa643254f4
gabystern <stern.gabrielle@gmail.com>
2017-07-18 15:07:01 -0400

    Cleaned up code

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Appointment = (props) => {
-  console.log(props)
 
   return(
     <div className='appt'><strong>{props.appointment.name}</strong><br></br>{props.appointment.location}</div>
